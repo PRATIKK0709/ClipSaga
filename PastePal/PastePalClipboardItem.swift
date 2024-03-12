b98143a052c32ed327873eab5ef64ce90514638d
Syskey SK <139443204+PRATIKK0709@users.noreply.github.com>
2024-03-12 21:21:13 +0530

    Update PastePalClipboardItem.swift

--- PastePal/PastePalClipboardItem.swift
+++ PastePal/PastePalClipboardItem.swift
@@ -13,10 +13,18 @@ struct ClipboardItem: Identifiable, Codable {
     let content: String
     var timestamp: Date
     var copiedFrom: String?
+    var contentType: ContentType
 
-    init(content: String, timestamp: Date, copiedFrom: String? = nil) {
+    enum ContentType: String, Codable {
+        case text
+        case image
+        case gif
+    }
+
+    init(content: String, timestamp: Date, copiedFrom: String? = nil, contentType: ContentType = .text) {
         self.content = content
         self.timestamp = timestamp
         self.copiedFrom = copiedFrom
+        self.contentType = contentType
     }
 }

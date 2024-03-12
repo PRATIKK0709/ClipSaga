51e56d48d76393e518fc5ad891171de9c11d7fc5
Syskey SK <139443204+PRATIKK0709@users.noreply.github.com>
2024-03-12 21:21:32 +0530

    Update PastePalManager.swift

--- PastePal/PastePalManager.swift
+++ PastePal/PastePalManager.swift
@@ -10,63 +10,93 @@ import SwiftUI
 
 class PastePalManager: ObservableObject {
     @Published var clipboardItems: [ClipboardItem] = []
-
+    
+    private var clipboardCheckTimer: Timer?
+    
     init() {
         startClipboardMonitoring()
         loadClipboardItems()
     }
-
+    
     private func startClipboardMonitoring() {
-        NotificationCenter.default.addObserver(self, selector: #selector(handleClipboardChange), name: NSApplication.didBecomeActiveNotification, object: nil)
+        clipboardCheckTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(checkClipboard), userInfo: nil, repeats: true)
     }
-
+    
+    @objc private func checkClipboard() {
+        if let newContent = NSPasteboard.general.string(forType: .string) {
+            let contentType: ClipboardItem.ContentType = newContent.hasSuffix(".gif") ? .gif : .text
+            addItem(content: newContent, contentType: contentType)
+        } else if let imageData = NSPasteboard.general.data(forType: .tiff), let _ = NSImage(data: imageData) {
+            // Assume any image data as image content
+            addItem(content: "Image", contentType: .image)
+        } else if let url = NSPasteboard.general.string(forType: .URL), url.lowercased().hasSuffix(".gif") {
+            // Assume a URL ending with ".gif" as GIF content
+            addItem(content: url, contentType: .gif)
+        }
+    }
+    
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
-
+    
     @objc private func handleClipboardChange() {
         if let newContent = NSPasteboard.general.string(forType: .string) {
-            addItem(content: newContent)
+            let contentType: ClipboardItem.ContentType = newContent.hasSuffix(".gif") ? .gif : .text
+            addItem(content: newContent, contentType: contentType)
+        } else if let imageData = NSPasteboard.general.data(forType: .tiff), let _ = NSImage(data: imageData) {
+            // Assume any image data as image content
+            addItem(content: "Image", contentType: .image)
+        } else if let url = NSPasteboard.general.string(forType: .URL), url.lowercased().hasSuffix(".gif") {
+            // Assume a URL ending with ".gif" as GIF content
+            addItem(content: url, contentType: .gif)
         }
     }
-
+    
     private func loadClipboardItems() {
         if let data = UserDefaults.standard.data(forKey: "clipboardItems"),
            let loadedItems = try? JSONDecoder().decode([ClipboardItem].self, from: data) {
             clipboardItems = loadedItems
         }
     }
-
+    
     private func saveClipboardItems() {
         if let encodedData = try? JSONEncoder().encode(clipboardItems) {
             UserDefaults.standard.set(encodedData, forKey: "clipboardItems")
         }
+        
     }
-
-    func addItem(content: String, copiedFrom: String? = nil) {
+    
+    func addItem(content: String, copiedFrom: String? = nil, contentType: ClipboardItem.ContentType = .text) {
         if let existingItemIndex = clipboardItems.firstIndex(where: { $0.content == content }) {
             clipboardItems[existingItemIndex].timestamp = Date()
             clipboardItems[existingItemIndex].copiedFrom = copiedFrom
         } else {
-            let newItem = ClipboardItem(content: content, timestamp: Date(), copiedFrom: copiedFrom)
+            let newItem = ClipboardItem(content: content, timestamp: Date(), copiedFrom: copiedFrom, contentType: contentType)
             clipboardItems.insert(newItem, at: 0)
         }
-        saveClipboardItems()
     }
-
+    
     func copyToClipboard(_ content: String) {
         NSPasteboard.general.clearContents()
         NSPasteboard.general.writeObjects([content as NSString])
     }
-
+    
     func deleteClipboardItem(_ item: ClipboardItem) {
         guard let index = clipboardItems.firstIndex(where: { $0.id == item.id }) else { return }
         clipboardItems.remove(at: index)
         saveClipboardItems()
+        
+        // Clear the clipboard content after saving the changes
+        NSPasteboard.general.clearContents()
     }
 
+    
+    
     func clearClipboard() {
         clipboardItems.removeAll()
         saveClipboardItems()
+        
+        // Clear the clipboard content after saving the changes
+        NSPasteboard.general.clearContents()
     }
 }
